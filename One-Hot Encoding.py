d5f2b96541feac7915256df36c21501a05785650
Jeeva261 <js3316573@gmail.com>
2024-09-17 15:45:09 +0530

    One hot encoding.py

--- One-Hot Encoding.py
+++ One-Hot Encoding.py
@@ -1,3 +1,21 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 
+file_path=r'C:\Users\ADMIN\OneDrive\Desktop\AIML githup\Encoding-Categorical-Data\file.csv'
+
+df=pd.read_csv(file_path)
+encoding=OneHotEncoder(sparse_output=False)
+df=encoding.fit_transform(df[["Name"]])
+print(df)
+
+df=pd.read_csv(file_path)
+encoding=OneHotEncoder(sparse_output=False)
+new_encoding=encoding.fit_transform(df[["Name"]])
+new_encoding_df=pd.DataFrame(new_encoding,columns=encoding.get_feature_names_out(["Name"]))
+print(new_encoding_df)
+
+# dummy encoding
+
+df=pd.read_csv(file_path)
+df=pd.get_dummies(df["Name"])
+print(df)
